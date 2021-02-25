53af6c56fa3b3df4b2b1c2b73379b51b1ede6942
Federico Jose Lucia <federicojoselucia@gmail.com>
2021-02-24 22:08:38 -0300

    Initial Commit

---
+++ tryGoogle.js
@@ -0,0 +1,16 @@
+var params = new URLSearchParams(window.location.search);
+
+if(params.has("q")){
+
+    var searchText = params.get("q");
+    var googleUri = "https://google.com/search?q=" + encodeURIComponent(searchText);
+
+    var anchor = $("<a>");
+    anchor.attr("href", googleUri);
+    anchor.attr("title","Try using Google");
+    anchor.attr("target","_blank");
+    anchor.attr("rel","noopener nofollow noreferrer");
+    anchor.text("Try Google");
+
+    $(".search-filters").append(anchor);
+}
