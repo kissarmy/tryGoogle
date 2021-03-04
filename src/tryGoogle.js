10ed70e8bb64b20bb1014854646f907a0ea00fef
Federico Jose Lucia <federicojoselucia@gmail.com>
2021-03-03 21:44:39 -0300

    Folder Organization & Licence

--- src/tryGoogle.js
+++ src/tryGoogle.js
@@ -1,15 +1,15 @@
 var params = new URLSearchParams(window.location.search);
 
-if(params.has("q")){
+if (params.has("q")) {
 
     var searchText = params.get("q");
     var googleUri = "https://google.com/search?q=" + encodeURIComponent(searchText);
 
     var anchor = $("<a>");
     anchor.attr("href", googleUri);
-    anchor.attr("title","Try using Google");
-    anchor.attr("target","_blank");
-    anchor.attr("rel","noopener nofollow noreferrer");
+    anchor.attr("title", "Try using Google");
+    anchor.attr("target", "_blank");
+    anchor.attr("rel", "noopener nofollow noreferrer");
     anchor.text("Try Google");
 
     $(".search-filters").append(anchor);
